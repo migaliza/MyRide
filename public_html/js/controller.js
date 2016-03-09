c152c36775a651c35e10a38fa33e80abbd363ea5
migaliza <migaliza.lyza@gmail.com>
2016-03-09 23:15:27 +0000

    added admin login

--- public_html/js/controller.js
+++ public_html/js/controller.js
@@ -14,25 +14,11 @@ $( document ).ready(function(){
     $('.modal-trigger').leanModal();
   });
   
- 
-  /*
-function initMap() {
-  var accra = {lat: 5.7500, lng: 0.0000};
-  //var indianapolis = {lat: 39.79, lng: -86.14};
-
-  var map = new google.maps.Map(document.getElementById('mainPage'), {
-    center: accra,
-    scrollwheel: false,
-    zoom: 7
+  /**
+   * function to add event on the collapsible menu in the Admindashboard
+   */
+ $(document).ready(function(){
+    $('.collapsible').collapsible({
+      //accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
   });
-
-  var directionsDisplay = new google.maps.DirectionsRenderer({
-    map: map
-  });*/
-
-  /*// Set destination, origin and travel mode.
-  var request = {
-    destination: indianapolis,
-    origin: chicago,
-    travelMode: google.maps.TravelMode.DRIVING*/
-  //};
