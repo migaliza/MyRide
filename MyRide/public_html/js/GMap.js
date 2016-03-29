43d9a20b1531821a63c178febfa860b19ddb4b16
migaliza <migaliza.lyza@gmail.com>
2016-03-29 19:30:21 +0000

    Location direction working perfectly and also the various busses can show

--- MyRide/public_html/js/GMap.js
+++ MyRide/public_html/js/GMap.js
@@ -5,128 +5,218 @@
  */
 
 
- $(document).ready(function(){
-      if(navigator.geolocation){
-         navigator.geolocation.getCurrentPosition(initMap);
-         
-     }
-     else{
-         error("Geo Location is not supported");
-     }
-  })
- 
- /*
-  * function to draw the current location of the map
-  * 
-  */
- 
- function initMap(position){
-     var coords = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-     
-     var options={
-         zoom: 12,
-         center: coords,
-         scrollwheel: true,
-         mapTypeControlOptions:{
-             style: google.maps.NavigationControlStyle.SMALL
-         },
-         
-         //mapTypeId: google.maps.MapTypeId.ROADMAP
-         
-     };
-     
-     var map = new google.maps.Map(document.getElementById('MapCanvas'),options);
-     
-     
-     var marker = new google.maps.Marker({
-         position:coords,
-         map:map,
-         title:"Your are here"
-     });
-     
-     //create a search box and link it to the elements
-     var input = document.getElementById('findPlaces');
-     var options ={
-         types:['locality']
-     };
-     var searchBox = new google.maps.places.SearchBox(input);
-     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-     
-     //Bias the searchBox results towards the current mapp view point\
-     map.addListener('places_changed',function(){
-         searchBox.setBounds(map.getBounds());
-     })
-     
-     //listen to the event fired when user selectes a prediction and
-     //retrieves
-     var markers= [];
-     searchBox.addListener('places_changed',function(){
-        var places = searchBox.getPlaces();
-        
-        if(places.length==0){
+/*
+ * function to send request
+ * @param {type} directionsService
+ * @param {type} directionsDisplay
+ * @returns {undefined}
+ */
+function sendRequest(u) {
+    //alert("here");
+    // alert(u);
+    console.log(u);
+    var obj = $.ajax({url: u, async: false});
+    var result = $.parseJSON(obj.responseText);
+    return result;
+}
+
+/* global google */
+
+var directionsDisplay = new google.maps.DirectionsRenderer();
+var directionsService = new google.maps.DirectionsService();
+$(document).ready(function () {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(initMap);
+
+    }
+    else {
+        error("Geo Location is not supported");
+    }
+
+
+});
+
+/*
+ * function to draw the current location of the map
+ * 
+ */
+
+
+var coords;
+function initMap(position) {
+    var destination_place_id = null;
+    var origin_place_id = null;
+    var travel_mode = google.maps.TravelMode.DRIVING;
+
+    coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var options = {
+        zoom: 12,
+        center: coords,
+        scrollwheel: true,
+        mapTypeControlOptions: {
+            style: google.maps.NavigationControlStyle.SMALL
+        }
+    };
+
+    var map = new google.maps.Map(document.getElementById('MapCanvas'), options);
+
+
+    /*
+     * marker to show current position of user
+     * @type google.maps.Marker
+     */
+   var marker = new google.maps.Marker({
+        position: coords,
+        map: map,
+        title: "Your are here"
+    });
+    
+    
+
+    /*
+     * function to place buses on the map
+     * @param {type} directionsService
+     * @param {type} directionsDisplay
+     * @returns {undefined}
+     */
+
+    function traceBusOnMap() {
+        var theURL = "http://166.62.103.147/~ashesics/class2016/beatrice_migaliza/MyRide/public_html/PHP/request.php?cmd=2";
+        var obj = sendRequest(theURL);
+        if (obj.result === 1) {
+            $.each(obj.coordinates, function (i, coordinates) {
+                var bus = coordinates.Device_Id;
+                var longitude = coordinates.long;
+                var latitude = coordinates.lat;
+                var info = '<div id="content"><a href="#">' + bus + '</a></div>';
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: info
+                });
+                //var marker;
+                var busIcon = new google.maps.MarkerImage("images/logo.png", null, null, null, new google.maps.Size(40, 40));
+               
+
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: new google.maps.LatLng(latitude, longitude),
+                    icon: busIcon,
+                    title: bus
+                });
+                
+                marker.addListener('click', function () {
+                    infowindow.open(map, marker);
+                });
+
+            });
+        }
+
+    }
+
+    traceBusOnMap();
+
+
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    directionsDisplay.setMap(map);
+    
+    var origin = document.getElementById('start');
+    var destination = document.getElementById('end');
+
+    // push the destination input text box on the map
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(origin);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(destination);
+
+    //add autocompletion on the search text
+    var origin_autocomplete = new google.maps.places.Autocomplete((origin));
+    origin_autocomplete.bindTo('bounds', map);
+    var destination_autoComplete = new google.maps.places.Autocomplete(destination);
+    destination_autoComplete.bindTo('bounds', map);
+    
+    
+    /**
+     * function to fit the viewport on the screen
+     * @param {type} map
+     * @param {type} place
+     * @returns {undefined}
+     */
+    function expandviewport(map, place) {
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        }
+        else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+    }
+    //expandviewport(map,place);
+    /*
+     * eventlistener for any change carried on on the map
+     */
+
+    origin_autocomplete.addListener('place_changed', function () {
+        //alert("at origin autocomplete");
+        var place = origin_autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("contained no geometry");
             return;
         }
+        expandviewport(map, place);
         
-        //clear the old markers
-        markers.forEach(function(marker){
-            marker.setMap(null);
+        /* if the place has geometry store its place id and route it if there is the other place id */
+        origin_place_id = place.place_id;
+       // alert(origin_place_id);
+        route(origin_place_id, destination_place_id, travel_mode, directionsService, directionsDisplay);
+    });
+
+    destination_autoComplete.addListener('place_changed', function () {
+        //alert("at destination autocomplete");
+        var place = destination_autoComplete.getPlace();
+        if (!place.geometry) {
+            window.alert("Contained no geometry");
+            return;
+        }
+        expandviewport(map, place);
+
+        /* if the place has geometry, store it and route it and route it if there is the other place id */
+        destination_place_id = place.place_id;
+        route(origin_place_id, destination_place_id, travel_mode, directionsService, directionsDisplay);
+
+    });
+
+
+    /**
+     * function to draw the route from origin to destination 
+     * @param {type} origin_place_id
+     * @param {type} destination_place_id
+     * @param {type} travel_mode
+     * @param {type} directionsService
+     * @param {type} directionsDisplay
+     * @returns {undefined}
+     */
+    function route(origin_place_id, destination_place_id, travel_mode, directionsService, directionsDisplay) {
+        if (!origin_place_id || !destination_place_id) {
+            return;
+        }
+
+        directionsService.route({
+            origin: {'placeId': origin_place_id},
+            destination: {'placeId': destination_place_id},
+            travelMode: travel_mode},
+        function (response, status) {
+            if (status === google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            }
+            else {
+                window.alert('request failed');
+            }
         });
-        
-        markers = [];
-        //for each place get the icon and name of the place
-        var bounds = new google.maps.LatLngBounds();
-        places.forEach(function(place){
-            var icon = {
-                url: place.icon,
-                size: new google.maps.Size(71,71),
-                origin: new google.maps.Point(0,0),
-                anchor: new google.maps.Point(17,34),
-                scaledSize: new google.maps.Size(25,25)
-                
-            };
-            
-            //create a marker for eah place
-            markers.push(new google.maps.Marker({
-                map:map,
-                icon:icon,
-                title:place.name,
-                position:place.geometry.location
-            }));
-            //map.fitBounds(bounds);
-        });
-     });
-    
- }
- /*
- function initMap() {
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('MapCanvas'), {
-    center: {lat: 5.5500, lng: 0.2000},
-    scrollwheel: false,
-    zoom: 8
-  });
-  var infoWindow = new google.maps.InfoWindow({MapCanvas:MapCanvas});
-  //htm5 geolocation
-  if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(function(position){
-          var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-          };
-          
-          infoWindow.setPosition(pos);
-          //infoWindow.setContent('')
-          map.setCenter(pos);
-      },function(){
-          handlelocationError(tru,infoWindo,map.getCenter());
-  
-          
-      });
-      
-  }
-  
-  else{
-      handleLocationError(false,inforWindow,map.getCenter());
-  }
+    }
 }
-*/
+
+
+
+
+
+
+
